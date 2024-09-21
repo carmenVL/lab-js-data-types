0b7a06e17b80e8bfaf5911debff6dc097d3ca9a3
Carmen Valderrama <cvalderrama@ISGLOBAL.LAN>
2024-09-21 14:00:24 +0200

    ejercicio

--- index.js
+++ index.js
@@ -8,11 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const tongueTwister ='${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}'
 
 // Print out the concatenated string
-
-
+console.log(tongueTwister)
 
 
 /*******************************************
@@ -21,10 +20,15 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
-// Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+ 
+// Get the first character and uppercase it
+const cameLtaiLPart1 = part1.substring(0, part1.length - 1) + part1[part1.length - 1].toUpperCase();
+const cameLtaiLPart2 = part2.substring(0, part2.length - 1) + part2[part2.length - 1].toUpperCase();
 
-
-// Print the cameLtaiL-formatted string
+// Concatenate uppercased letter and the rest of the string
+const result = cameLtaiLPart1 + cameLtaiLPart2;
+ 
+console.log(result);
 
 
 
@@ -35,10 +39,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const tipAmount = billTotal * 0.15
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,9 +51,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random() * 10) + 1;
 
 // Print the generated random number
+console.log(randomNumber);
 
 
 
@@ -62,15 +67,22 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// false
 
 const expression2 = a || b;
+//true
 
 const expression3 = !a && b;
+//false
 
 const expression4 = !(a && b);
+//true
 
 const expression5 = !a || !b;
+//true
 
 const expression6 = !(a || b);
+//false
 
 const expression7 = a && a;
+//true
